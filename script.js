4bad57f12dfbe2770bb9313143a5bda7de06e521
cesarsiguencia <cesar.siguencia@gmail.com>
2024-06-09 22:52:25 -0400

    successfully implemented localstorage capability

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var sectionHolder = document.querySelector("#scheduler-content")
+var button = document.querySelector("#save-task")
 var taskIdCounter = 0
-console.log('working')
+
 var sections = [
     {
         title: 'Work'
@@ -21,11 +22,11 @@ const generateSections = () =>{
         sectionLoad.innerHTML = `
             <h3>${section.title}</h3>
             <div class="container-fluid py-1">
-                <div class="row my-4" task-id=${section.title +'-'+0}>
-                    <input class="col" task-id=${section.title +'-'+0}>
+                <div class="row my-4" task-type=${section.title} task-id=${section.title +'-'+0}>
+                    <input class="col" task-type=${section.title} task-id=${section.title +'-'+0}>
 
                     </input>
-                    <select name="Weekday Due" class="col-2" task-id=${section.title +'-'+0}>
+                    <select name="Weekday Due" class="col-2" task-type=${section.title} task-id=${section.title +'-'+0}>
                         <option value="" disabled selected>Due Date</option>
                         <option value="monday">Monday</option>
                         <option value="tuesday">Tuesday</option>
@@ -36,17 +37,17 @@ const generateSections = () =>{
                         <option value="sunday">Sunday</option>
                         <option value="next week">Next Week</option>
                     </select>
-                    <div class="col-1" task-id=${section.title +'-'+0}>
+                    <div class="col-1" task-type=${section.title} task-id=${section.title +'-'+0}>
                         <p></p>
                     </div>
                 </div>
                 <div class="divider"></div>
 
-                <div class="row my-4" task-id=${section.title +'-'+1}>
-                    <input class="col" task-id=${section.title +'-'+1}>
+                <div class="row my-4" task-type=${section.title} task-id=${section.title +'-'+1}>
+                    <input class="col" task-type=${section.title} task-id=${section.title +'-'+1}>
 
                     </input>
-                    <select name="Weekday Due" class="col-2" task-id=${section.title +'-'+0}>
+                    <select name="Weekday Due" class="col-2" task-type=${section.title} task-id=${section.title +'-'+1}>
                         <option value="" disabled selected>Due Date</option>
                         <option value="monday">Monday</option>
                         <option value="tuesday">Tuesday</option>
@@ -57,17 +58,17 @@ const generateSections = () =>{
                         <option value="sunday">Sunday</option>
                         <option value="next week">Next Week</option>
                     </select>
-                    <div class="col-1" task-id=${section.title +'-'+1}>
+                    <div class="col-1" task-type=${section.title} task-id=${section.title +'-'+1}>
                         <p></p>
                     </div>
                 </div>
                 <div class="divider"></div>
 
-                <div class="row my-4" task-id=${section.title +'-'+2}>
-                    <input class="col" task-id=${section.title +'-'+2}>
+                <div class="row my-4" task-type=${section.title} task-id=${section.title +'-'+2}>
+                    <input class="col" task-type=${section.title} task-id=${section.title +'-'+2}>
       
                     </input>
-                    <select name="Weekday Due" class="col-2" task-id=${section.title +'-'+0}>
+                    <select name="Weekday Due" class="col-2" task-type=${section.title} task-id=${section.title +'-'+2}>
                         <option value="" disabled selected>Due Date</option>
                         <option value="monday">Monday</option>
                         <option value="tuesday">Tuesday</option>
@@ -78,7 +79,7 @@ const generateSections = () =>{
                         <option value="sunday">Sunday</option>
                         <option value="next week">Next Week</option>
                     </select>
-                    <div class="col-1" task-id=${section.title +'-'+2}>
+                    <div class="col-1" task-type=${section.title} task-id=${section.title +'-'+2}>
                         <p></p>
                     </div>
                 </div>
@@ -88,50 +89,58 @@ const generateSections = () =>{
         sectionHolder.appendChild(sectionLoad)
     })
 }
-
-
-
 generateSections()
 
-var userData = [
-    {
-        type: "Personal",
-        taskId: "Personal-0",
-        content: 'I have to praise God',
-        day: 'friday',
-        completed: true
-    }
-]
+// var userData = [
+//     {
+//         type: "Personal",
+//         taskTag: "Personal-0",
+//         content: 'Working example',
+//         day: 'friday',
+//         completed: true
+//     },
+//     {
+//         type: "Work",
+//         taskTag: "Work-1",
+//         content: 'Another one',
+//         day: 'tuesday',
+//         completed: false
+//     }
+// ]
+
+var userData = []
 
 var loadUserData = () =>{
-    userData.forEach((task)=>{
-        var pickedBlockContent = document.querySelector(".col[task-id='" + task.taskId + "']")
-        pickedBlockContent.value = task.content
-
-        var pickedBlockDate = document.querySelector(".col-2[task-id='" + task.taskId + "']")
-        pickedBlockDate.value = task.day
-
-        var pickedBlockComplete = document.querySelector(".col-1[task-id='" + task.taskId + "']")
-        if(task.completed === true){
-            pickedBlockComplete.className = "complete col-1"
-        }
-        
-    })
+    var newArray = JSON.parse(localStorage.getItem("scheduler-tasks"))
+    if(newArray){
+        userData = newArray
+    }
+    
+    console.log(userData)
+    if(userData){
+        userData.forEach((task)=>{
+            var pickedBlockContent = document.querySelector(".col[task-id='" + task.taskTag + "']")
+            pickedBlockContent.value = task.content
+    
+            var pickedBlockDate = document.querySelector(".col-2[task-id='" + task.taskTag + "']")
+            pickedBlockDate.value = task.day
+    
+            var pickedBlockComplete = document.querySelector(".col-1[task-id='" + task.taskTag + "']")
+            if(task.completed === true){
+                pickedBlockComplete.className = "complete col-1"
+            }
+        })
+    }
 }
-
 loadUserData()
 
-
 const editBlock = (e) =>{
-    if(e.target.matches(".col")){
-        var taskId = e.target.getAttribute("task-id")
-        var editBlockContent = document.querySelector(".col[task-id='" + taskId + "']").value
-
-        console.log(editBlockContent)
-    }
+    console.log('edits')
+    var taskType = e.target.getAttribute("task-type")
+    var taskId = e.target.getAttribute("task-id")
+    console.log(taskId)
 
     if(e.target.matches(".col-1")){
-        var taskId = e.target.getAttribute("task-id")
         var editBlockComplete = document.querySelector(".col-1[task-id='" + taskId + "']")
 
         if(editBlockComplete.hasAttribute("status")){
@@ -141,10 +150,40 @@ const editBlock = (e) =>{
             editBlockComplete.className = "complete col-1"
             editBlockComplete.setAttribute('status', "true")
         }
-
-
-
     }
+
+    sectionHolder.addEventListener("change", function(){
+        var editBlockContent = document.querySelector(".col[task-id='" + taskId + "']").value
+        var editBlockDay = document.querySelector(".col-2[task-id='" + taskId + "']").value
+        if(userData.length > 0){
+            userData.forEach((task)=>{
+                if(task.taskTag === taskId){
+                    const index = userData.indexOf(task)
+                    userData.splice(index, 1)
+                }
+            })
+        }
+        console.log(editBlockContent, 'outside')
+        if(editBlockContent && editBlockDay){
+            console.log(editBlockContent)
+            console.log(editBlockDay)
+            userData.push({
+                type: taskType,
+                taskTag: taskId,
+                content: editBlockContent,
+                day: editBlockDay
+                // completed: 
+            })
+        } 
+
+        // if(editBlockContent === ""){
+        //     console.log('no content')
+        // }
+
+
+        console.log(userData)
+        localStorage.setItem("scheduler-tasks", JSON.stringify(userData))
+    })
 }
 
 sectionHolder.addEventListener("click", editBlock)
